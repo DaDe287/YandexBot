2adbeac8be6715b59fb41bb684b480a346e0fada
DaDe287 <anatoli.voronov.62@gmail.com>
2025-09-27 13:10:16 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+docker-compose up
+
+python main.py
---
+++ lexicon/lexicon.py
@@ -0,0 +1,197 @@
+LEXICON = {
+    "ru": {
+        "start": "В какой стране ты будешь работать?",
+        "chosen": "Выбери язык бота / Choose bot language",
+    },
+    "az": {
+        "start": "Hansı ölkədə işləyəcəksiniz?",
+        "chosen": "Dil seçin",
+    },
+    "uz": {
+        "start": "Qaysi mamlakatda ishlaysiz?",
+        "chosen": "Tilni tanlang",
+    },
+    "be": {
+        "start": "У якой краіне вы будзеце працаваць?",
+        "chosen": "Выберыце мову",
+    },
+    "kk": {
+        "start": "Қай елде жұмыс істейсіз?",
+        "chosen": "Тілді таңдаңыз",
+    },
+    "ky": {
+        "start": "Кайсы өлкөдө иштейсиз?",
+        "chosen": "Тилди тандаңыз",
+    },
+    "en": {
+        "start": "In which country will you work?",
+        "chosen": "Choose language",
+    }
+}
+
+LANG_MAP = {
+    "lang_ru": "ru",
+    "lang_ky": "ky",
+    "lang_uz": "uz",
+    "lang_kk": "kk",
+    "lang_be": "be",
+    "lang_en": "en",
+    "lang_az": "az"
+}
+
+COUNTRY_MESSAGES = {
+    "ru": """🟡 Яндекс Еда — доставка, которая приносит доход!
+🤝🏼 Мы ищем курьеров (пеших, вело и авто).
+
+Что вас ждёт:
+💵 Ежедневные выплаты и бонусы за заказы.
+🕒 Свободный график: вы сами выбираете, когда работать.
+🛴 Работа на любом транспорте: пешком, велосипед, мото/авто.
+♥️ Все чаевые — ваши.
+💼 Всё необходимое для доставки получите в курьерском центре.
+🚀 Доплата за тяжелые заказы.
+👥 Бонусы за приглашённых курьеров.
+🎯 Работать без начальства.
+
+📲 Как стать курьером:
+
+1️⃣Заполните короткую анкету.
+
+2️⃣Выберите время и адрес офиса для посещения.
+
+3️⃣ Получите сумку и всё необходимое.
+
+4️⃣ Начинайте доставлять заказы.
+
+Написать оператору — @sabikuryer""",
+    "ky": """🟡 Яндекс Еда — жеткирүү, ал киреше алып келет!
+🤝🏼 Биз курьерлерди (аяк менен, велосипед, авто) издейбиз.
+
+Сизди эмне күтөт:
+💵 Күн сайын төлөмдөр жана заказдар үчүн бонустар.
+🕒 Ыңгайлуу график: качан иштегиңизди өзүңүз тандайсыз.
+🛴 Ар кандай транспорт менен иштөө: бут менен, велосипед, мото/авто.
+♥️ Бардык ченемдер — сиздики.
+💼 Жеткирүү үчүн керектүү нерселерди курьер борборунан аласыз.
+🚀 Ажыратуу кыйын болгон заказдар үчүн кошумча төлөм.
+👥 Досторду чакырганга бонустар.
+🎯 Боштукта иштөө, жетекчи жок.
+
+📲 Курьер болуунун жолу:
+
+1️⃣ Кыскача анкетаны толтуруңуз.
+2️⃣ Офистин убактысын жана дарегин тандаңыз.
+3️⃣ Сумка жана керектүү нерселерди алыңыз.
+4️⃣ Заказдарды жеткире баштаңыз.
+
+Операторго жазуу — @sabikuryer""",
+    "uz": """🟡 Yandex Eats — yetkazib berish, daromad keltiradi!
+🤝🏼 Biz kuryerlarni (oyoq bilan, velosiped, avtomobil) izlayapmiz.
+
+Sizni nima kutadi:
+💵 Har kuni to‘lovlar va buyurtmalar uchun bonuslar.
+🕒 Erkin jadval: qachon ishlashni o‘zingiz tanlaysiz.
+🛴 Har qanday transport bilan ishlash: oyoq bilan, velosiped, moto/avto.
+♥️ Barcha maslahatlar — sizniki.
+💼 Yetkazib berish uchun kerakli barcha narsalarni kuryer markazidan olasiz.
+🚀 Og‘ir buyurtmalar uchun qo‘shimcha to‘lov.
+👥 Taklif qilgan kuryerlar uchun bonuslar.
+🎯 Raxbariyatsiz ishlash.
+
+📲 Kuryer bo‘lish uchun:
+
+1️⃣ Qisqa so‘rovnomani to‘ldiring.
+2️⃣ Ofis uchun vaqt va manzilni tanlang.
+3️⃣ Sumka va kerakli barcha narsalarni oling.
+4️⃣ Buyurtmalarni yetkazishni boshlang.
+
+Operatorga yozing — @sabikuryer""",
+    "be": """🟡 Яндекс Еда — доставка, которая приносит доход!
+🤝🏼 Мы ищем курьеров (пеших, вело и авто).
+
+Что вас ждёт:
+💵 Ежедневные выплаты и бонусы за заказы.
+🕒 Свободный график: вы сами выбираете, когда работать.
+🛴 Работа на любом транспорте: пешком, велосипед, мото/авто.
+♥️ Все чаевые — ваши.
+💼 Всё необходимое для доставки получите в курьерском центре.
+🚀 Доплата за тяжелые заказы.
+👥 Бонусы за приглашённых курьеров.
+🎯 Работать без начальства.
+
+📲 Как стать курьером:
+
+1️⃣Заполните короткую анкету.
+
+2️⃣Выберите время и адрес офиса для посещения.
+
+3️⃣ Получите сумку и всё необходимое.
+
+4️⃣ Начинайте доставлять заказы.
+
+Написать оператору — @sabikuryer""",
+    "kk": """🟡 Яндекс Еда — табыс әкелетін жеткізу қызметі!
+🤝🏼 Біз курьерлерді іздейміз (жаяу, велосипедпен, мотоциклмен немесе автокөлікпен).
+
+Сізді не күтіп тұр:
+💵 Күнделікті төлемдер және тапсырыстар үшін бонустар.
+🕒 Еркін кесте: қашан жұмыс істегіңіз келетінін өзіңіз таңдайсыз.
+🛴 Кез келген көлік түрімен жұмыс істеу мүмкіндігі: жаяу, велосипед, мото/авто.
+♥️ Барлық шайпұлдар сізге қалады.
+💼 Жұмысқа қажет барлық заттарды курьерлік орталықтан аласыз.
+🚀 Қиын тапсырыстар үшін қосымша ақы.
+👥 Шақырған курьерлеріңіз үшін бонустар.
+🎯 Басшысыз жұмыс істеу еркіндігі.
+
+📲 Курьер болу үшін:
+1️⃣ Қысқа анкетаны толтырыңыз.
+2️⃣ Кеңсеге ыңғайлы уақыт пен мекенжайды таңдаңыз.
+3️⃣ Сөмке мен қажет заттарды алыңыз.
+4️⃣ Тапсырыстарды жеткізе бастаңыз!
+
+Операторға жазыңыз — @sabikuryer""",
+"en": """🟡 Yandex Eats — delivery that brings income!
+🤝🏼 We are looking for couriers (on foot, by bike, motorcycle, or car).
+
+What awaits you:
+💵 Daily payments and bonuses for completed orders.
+🕒 Flexible schedule: you choose when to work.
+🛴 Work with any transport: on foot, bicycle, motorcycle, or car.
+♥️ All tips are yours.
+💼 You’ll get everything you need for deliveries at the courier center.
+🚀 Extra pay for heavy orders.
+👥 Bonuses for inviting new couriers.
+🎯 Work without a boss.
+
+📲 How to become a courier:
+
+1️⃣ Fill out a short form.
+2️⃣ Choose a time and office address to visit.
+3️⃣ Get a delivery bag and everything you need.
+4️⃣ Start delivering orders.
+
+📩 Contact the operator — @sabikuryer""",
+
+    "az": """🟡 Yandex Eats — çatdırılma, gəlir gətirir!
+🤝🏼 Biz kuryerlər (ayaqla, velosiped, avtomobil) axtarırıq.
+
+Sizi nə gözləyir:
+💵 Hər gün ödəniş və sifarişlər üçün bonuslar.
+🕒 Sərbəst qrafik: işləmək istədiyiniz zamanı özünüz seçirsiniz.
+🛴 Hər cür nəqliyyat vasitəsi ilə işləmək: ayaqla, velosiped, moto/avto.
+♥️ Bütün uşaq pulları sizə məxsusdur.
+💼 Çatdırılma üçün lazım olan bütün əşyaları kuryer mərkəzindən əldə edəcəksiniz.
+🚀 Çətin sifarişlər üçün əlavə ödəniş.
+👥 Dəvət etdiyiniz kuryerlər üçün bonuslar.
+🎯 Rəhbərsiz işləmək.
+
+📲 Kuryer olmaq üçün:
+
+1️⃣ Qısa anketi doldurun.
+2️⃣ Ofis üçün vaxt və ünvan seçin.
+3️⃣ Çanta və lazım olan bütün əşyaları alın.
+4️⃣ Sifarişləri çatdırmağa başlayın.
+
+Operatora yazın — @sabikuryer"""
+
+}
